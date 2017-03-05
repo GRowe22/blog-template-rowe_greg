313a3ee75e421df66f98f7bfc4255a452693c8b7
Greg Rowe <growe22@ufl.edu>
2017-03-05 15:46:11 -0600

    add style to index for sidebar and color

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <!-- Begin Section Container -->
 <section class="row">
-    <div class="twelve columns">
+    <div class="eight columns">
         <!-- BEGIN LOOP -->
         <?php
             if ( have_posts() ) {
@@ -20,6 +20,10 @@
             } // end if
         ?>
         <!-- END LOOP -->
+    </div>
+
+    <div class="four columns">
+        <?php get_sidebar(); ?>
     </div>
 </section>
 <!-- End Section Container -->
