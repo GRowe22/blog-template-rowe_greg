5f28ebb6b833b07f28627a29444d3798b568c6d1
Greg Rowe <growe22@ufl.edu>
2017-03-06 17:20:25 -0600

    create sidebar content and styling

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,27 @@
+<?php
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post(); ?>
+
+            <div class="row">
+                <div class="three columns sidebar-thumbnail">
+                    <?php if ( has_post_thumbnail() ) {
+                    the_post_thumbnail('thumbnail');
+                    }?>
+                </div>
+                <div class="nine columns">
+                    <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                </div>
+            </div>
+            <?php
+        } // end while
+    } // end if
+?>
+<!-- END LOOP -->
+
 <?php dynamic_sidebar('first-widget'); ?>
+
+<?php
+if ( has_post_thumbnail() ) {
+    the_post_thumbnail('thumbnail');
+} ?>
