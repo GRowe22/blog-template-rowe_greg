77a551ee64eb655270df557bdfff712a7e940c22
Greg Rowe <growe22@ufl.edu>
2017-03-11 12:35:26 -0600

    edit footer styling

--- footer.php
+++ footer.php
@@ -11,8 +11,14 @@
                 <?php dynamic_sidebar('footer-widget-one'); ?>
             </div>
 
-            <div class="five columns">
-                <?php dynamic_sidebar('footer-widget-two'); ?>
+            <div class="five columns footer-text">
+                <h4>Footer Content</h4>
+                <p>
+                    Paragraph-Large. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad.
+                </p>
+                <p>
+                    Paragraph-Large. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad.
+                </p>
             </div>
 
             <div class="four columns footer-calendar">
