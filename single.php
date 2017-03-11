8fa727c38f47a0b07682f788c05a1f6c570aeb49
Greg Rowe <growe22@ufl.edu>
2017-03-11 14:32:48 -0600

    create content and style for 404 page

--- single.php
+++ single.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
     <section class="row">
-        <div class="twelve columns">
+        <div class="nine columns single-page-content">
             <?php
                 if (have_posts()) :
                     while (have_posts()) : the_post();
-                    if ( has_post_thumbnail() ) { ?>
-                    <div class="post-thumbnail"><?php the_post_thumbnail('large'); ?></div>
-                <?php } ?>
+                    ?>
                     <h2><?php the_title(); ?></h2>
                     <?php the_content();
                     endwhile;
                 endif; ?>
+        </div>
+        <div class="three columns sidebar-margin">
+            <?php get_sidebar(); ?>
         </div>
     </section>
 <?php get_footer(); ?>
