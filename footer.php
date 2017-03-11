5e093e272c6bfaed7bfcf824817d9dd6e63b03f2
Greg Rowe <growe22@ufl.edu>
2017-03-10 18:52:32 -0600

    create footer content and style

--- footer.php
+++ footer.php
@@ -1,16 +1,24 @@
-    <footer class="row">
-        <div class="four columns">
-            <?php dynamic_sidebar('footer-widget-one'); ?>
+    <footer>
+        <div class="row footer-navigation">
+            <div class="offset-by-four eight columns">
+                <?php wp_nav_menu(array(
+                    'sort_column' => 'menu_order',
+                    'container_class' => 'blank-menu-header'
+                    ));?>
+            </div>
         </div>
+            <div class="two columns">
+                <?php dynamic_sidebar('footer-widget-one'); ?>
+            </div>
 
-        <div class="four columns">
-            <?php dynamic_sidebar('footer-widget-two'); ?>
-        </div>
+            <div class="five columns">
+                <?php dynamic_sidebar('footer-widget-two'); ?>
+            </div>
 
-        <div class="four columns">
-            <?php dynamic_sidebar('footer-widget-three'); ?>
-        </div>
-
+            <div class="four columns footer-calendar">
+                <?php dynamic_sidebar('footer-widget-three'); ?>
+            </div>
+    </div>
     </footer>
 </div><!-- End Container Div-->
 
